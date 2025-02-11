9e6ce124741aa3494abebc5f000278c90d7e7ad6
UAssylbek <154306656+UAssylbek@users.noreply.github.com>
2025-02-11 21:56:01 +0500

    mini fix

--- internal/controller/workout/list_workouts.go
+++ internal/controller/workout/list_workouts.go
@@ -6,6 +6,14 @@ import (
 	"workout-training-api/internal/types/controller"
 )
 
+type listWorkoutsResponse struct {
+	Workouts []Workout 
+}
+
 func (w *WorkoutController) ListWorkouts(ctx context.Context, req controller.ListWorkoutsReq) (controller.ListWorkoutsResp, error) {
 	panic("implement me")
+}
+
+func (r *listWorkoutsResponse) GetWorkouts() []Workout {
+	return r.Workouts
 }
