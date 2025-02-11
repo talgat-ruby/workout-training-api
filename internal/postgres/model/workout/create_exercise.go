2944a3f4d13475db5bfafff1a31dfad78a3f447d
Arnurkabdylkak <kabdylkakarnur@gmail.com>
2025-02-11 20:09:42 +0500

    feat: new funcs in postgres

--- internal/postgres/model/workout/create_exercise.go
+++ internal/postgres/model/workout/create_exercise.go
@@ -12,9 +12,13 @@ func (m *Workout) CreateExercise(ctx context.Context, req database.CreateExercis
 
 
 func (m *Workout) UpdateExercise(ctx context.Context, req  database.UpdateExerciseReq) (database.UpdateExerciseResp, error) {
-
+	panic("implement me")
 }
 
-func (m *Workout) DeleteExercise(ctx context.Context, req database.DeleteExerciseReq) (database.DeleteExerciseResp, error)
+func (m *Workout) DeleteExercise(ctx context.Context, req database.DeleteExerciseReq) (database.DeleteExerciseResp, error) {
+	panic("implement me")
+}
 
-func (m *Workout) ListExpense(ctx context.Context, req database.) (database.ListWorkoutResp, error)
+func (m *Workout) ListExpense(ctx context.Context, req database.) (database.ListWorkoutResp, error) {
+	panic("implement me")
+}
